fb2f9e3460ba5d5cfa064a87d6ad5f2d39841b73
Jonas Erbesdobler <jonas.erbesdobler@tum.de>
2024-02-05 22:09:25 +0100

    added M'4 kernel + grid for MLS interpolation

---
+++ jax_energy_spectrum/grid_setup.py
@@ -0,0 +1,48 @@
+"""Create grid for moving least squares interpolation"""
+
+import numpy as np
+from jax_sph.utils import pos_init_cartesian_2d, pos_init_cartesian_3d
+
+
+class interpolationGrid():
+        
+    def __init__(
+        self,
+        args,
+    ):
+        super().__init__(args)
+
+        self.nx, self.ny, self.nz = [int(i) for i in args.nxnynz.split("_")]
+        # length, heigth, and width of fluid domain; box with 3dx not incl.
+        dx = args.dx
+        self.L, self.H, self.W = self.nx * dx, self.ny * dx, self.nz * dx
+   
+    def initialize_grid(self):
+        args = self.args
+
+        # initialize box and regular grid positions of MLS interpolation grid
+        if args.dim == 2:
+            box_size = self._box_size2D()
+            r = self._init_pos2D(box_size, args.dx)
+        elif args.dim == 3:
+            box_size = self._box_size3D()
+            r = self._init_pos3D(box_size, args.dx)
+
+        num_interp_points = len(r)
+        print("Total number of interpolation grid ponts = ", num_interp_points)
+
+        return r
+
+    def _box_size2D(self):
+        wall = 0 if self.relax_pbc else 6
+        return (np.array([self.nx, self.ny]) + wall) * self.args.dx
+
+    def _box_size3D(self):
+        wall = 0 if self.relax_pbc else 6
+        return (np.array([self.nx, self.ny, self.nz]) + wall) * self.args.dx
+    
+    def _init_pos2D(self, box_size, dx):
+        return pos_init_cartesian_2d(box_size, dx)
+
+    def _init_pos3D(self, box_size, dx):
+        return pos_init_cartesian_3d(box_size, dx)
