f2802d57cd8778cced4d3f1e18c76e898af6cb14
Jonas Erbesdobler <jonas.erbesdobler@tum.de>
2024-02-07 08:17:53 +0100

    energy spectrum so far. Missing few things and FFT over velocity field

--- jax_energy_spectrum/energy_spectrumn.py
+++
@@ -1,9 +0,0 @@
-"""Energy Spectrum implementation"""
-
-import jax.numpy as jnp
-from jax import ops, vmap
-from jax_md import space
-
-from jax_sph.kernels import QuinticKernel
-from jax_sph.kernels import WendlandC2Kernel
-from jax_sph.kernels import M4PrimeKernel
